28cc7f31299bc7b53298f4c00fdaa159a7ba46a3
yto9 <ytoku.pro@gmail.com>
2018-08-11 13:31:57 +0900

    modify args for scripts

--- publish_drafts.sh
+++ publish_drafts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # usage ./publish_drafts.sh _drafts/*/*
-echo $1 | sed -E 's/\// /g'| awk '{print $1 "/" $2 "/"$3 " _posts/" $2 "-" $3}' | xargs -n 2 cp
+ echo $* | sed -E 's/\// /g'| sed -E 's/md /md\n/g'| awk '{print $1 "/" $2 "/"$3 " _posts/" $2 "-" $3}' | xargs -n 2 cp
